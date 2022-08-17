445272f4f48ee6e1473746bfef3f9768a0de07d6
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2022-08-17 08:02:15 -0700

    First release

---
+++ no-creeper-crators/src/main/java/io/github/stonley890/App.java
@@ -0,0 +1,24 @@
+package io.github.stonley890;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+        getLogger()
+                .info("For u/Lupich by u/Stonley890");
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onCreeperExplosion (EntityExplodeEvent event) {
+        event.blockList().clear();
+    }
+
+}
