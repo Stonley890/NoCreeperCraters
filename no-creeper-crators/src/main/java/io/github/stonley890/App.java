d582871995158d52f0c143405ac130124fc96765
Stonley890 <stonley@macver.org>
2024-08-10 16:56:06 -0700

    Creepers no longer destroy hanging entities (item frames, paintings).

--- no-creeper-crators/src/main/java/io/github/stonley890/App.java
+++ no-creeper-crators/src/main/java/io/github/stonley890/App.java
@@ -1,10 +1,13 @@
 package io.github.stonley890;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Creeper;
+import org.jetbrains.annotations.NotNull;
 
 public class App extends JavaPlugin implements Listener {
 
@@ -14,8 +17,14 @@ public class App extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onCreeperExplosion (EntityExplodeEvent event) {
+    public void onCreeperExplosion (@NotNull EntityExplodeEvent event) {
         if(event.getEntity() instanceof Creeper) event.blockList().clear();
+    }
+
+    @EventHandler
+    public void onEntityDamage (@NotNull HangingBreakByEntityEvent event) {
+        if (event.getRemover() != null && event.getRemover().getType() == EntityType.CREEPER)
+            event.setCancelled(true);
     }
 
 }
